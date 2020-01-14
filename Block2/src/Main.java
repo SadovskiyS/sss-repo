8428be68d0420af895b0052315b514f0f4b57e79
SadovskiyS <galleosss096@gmail.com>
2020-01-14 20:23:19 +0200

    2 lines

--- Block2/src/Main.java
+++ Block2/src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args){
         System.out.println("keks");
         System.out.println("lol");
-        System.out.println("arbidol");
     }
 }
